dff0cd86c68e7def64efc0e1a7e26ecf50ebb45a
aldoromano <tiano.roma@gmail.com>
2023-08-30 23:22:09 +0200

    WIP

--- containers/PlayScreen.js
+++ containers/PlayScreen.js
@@ -11,22 +11,46 @@ import Constants from "expo-constants";
 
 export default function () {
   const { params } = useRoute();
-
+  let tab = [];
+  for (let i = 0; i < 7; i++) {
+    tab.push(<View style={styles.view} key={i}></View>);
+  }
   //   console.log("params -> ", params);
-
+  /*
+  const renderRow = () => {
+    return (
+      <View style={{ flex: 1, alignSelf: "stretch", flexDirection: "row" }}>
+        <View style={{ flex: 1, alignSelf: "stretch" }} />{" "}
+        <View style={{ flex: 1, alignSelf: "stretch" }} />
+        <View style={{ flex: 1, alignSelf: "stretch" }} />
+        <View style={{ flex: 1, alignSelf: "stretch" }} />
+        <View style={{ flex: 1, alignSelf: "stretch" }} />
+      </View>
+    );
+  };
+  const data = [1, 2, 3, 4, 5];
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>This is the PLAYSCREEN component</Text>
+      {data.map((datum) => {
+        // This will render a row for each data element.
+        return renderRow();
+      })}
     </View>
   );
+  */
+  return tab;
 }
 
 const styles = StyleSheet.create({
+  view: {
+    backgroundColor: "lightgreen",
+    borderWidth: 2,
+    height: 100,
+    // width: 200,
+    width: Dimensions.get("window").width / 7,
+  },
+
   container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-    width: Dimensions.get("window").width,
+    flexDirection: "row",
   },
 });
